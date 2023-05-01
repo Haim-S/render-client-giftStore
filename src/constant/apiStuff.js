1a4e2273217f666e325eaf152f00cc03d0cf404a
HaimSlbah <26life72@gmail.com>
2023-05-01 15:49:14 +0300

    fix the url

--- src/constant/apiStuff.js
+++ src/constant/apiStuff.js
@@ -1,7 +1,7 @@
 
 export const END_POINT = "https://dummyjson.com/products";
-export const END_POINT_AUTH = "http://localhost:4000/auth";
-export const END_POINT_ORDER = "http://localhost:4000/order";
+export const END_POINT_AUTH = "https://giftstore-server.onrender.com/auth";
+export const END_POINT_ORDER = "https://giftstore-server.onrender.com/order";
 
 export const DEFAULT_OPTIONS = {
     headers: {'content-type': 'appliction/json'}
